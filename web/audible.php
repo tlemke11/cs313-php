ebbf241be1aff43b656228e876ac5da78c6964fa
tslemke11 <tyleris@gmail.com>
2016-10-22 20:25:53 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -8,5 +8,10 @@ require_once('dbconnection.php'); //include the db
 
   $sql = 'SELECT main_category_id, name FROM main_categories ORDER BY name';
   $mainCats = $connection->query($sql);
+  //http://php.net/manual/en/pdo.query.php
+  foreach ($connection->query($sql) as $row) {
+    print $row['main_category_id'] . " ";
+    print $row['name'] . "<br>";
+  }
   print_r($mainCats);
 ?>
