537ee36b4ef5124778c4d574a7b3dbc819faa1f3
tslemke11 <tyleris@gmail.com>
2016-10-22 21:18:36 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -7,29 +7,24 @@ require_once('dbconnection.php'); //include the db
 //this is gonna be a trip
 
   $main_cat_sql = 'SELECT main_category_id, name FROM main_categories ORDER BY name';
-  $sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=117 ORDER BY name";
+  $sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=$mainID ORDER BY name";
   $mainID = '';
-  
-  
-  //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
-  //See your common sense's answer
-  $subArray = $connection->prepare($sub_cat_sql);
-  $subArray->execute();
-  $subCats = $subArray->fetchAll();
-    
-  foreach ($subCats as $rows){
-      print $rows['sub_category_id'] . " ";
-      print $rows['name'] . "<br>";
-    }  
-  
+ 
   //http://php.net/manual/en/pdo.query.php
-  //foreach ($connection->query($main_cat_sql) as $row){
-    //print $row['main_category_id'] . " ";
-    //print $row['name'] . "<br>";
-    //$mainID = $row['main_category_id'];
-    //$subArray = $connection->prepare($sub_cat_sql);
-   // $subCats = $subArray->fetchAll();
-
-  //}
-
+  foreach ($connection->query($main_cat_sql) as $row){
+    print $row['main_category_id'] . " ";
+    print $row['name'] . "<br>";
+    $mainID = $row['main_category_id'];
+    
+    //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
+    //See your common sense's answer
+    $subArray = $connection->prepare($sub_cat_sql);
+    $subArray->execute();
+    $subCats = $subArray->fetchAll();
+    
+    foreach ($subCats as $rows){
+        print $rows['sub_category_id'] . " ";
+        print $rows['name'] . "<br>";
+      } 
+  }
 ?>
