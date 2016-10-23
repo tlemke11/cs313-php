c89614705bd5fa15b91b74c835f72e7e6956b319
tslemke11 <tyleris@gmail.com>
2016-10-22 21:05:18 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -9,14 +9,15 @@ require_once('dbconnection.php'); //include the db
   $main_cat_sql = 'SELECT main_category_id, name FROM main_categories ORDER BY name';
   $sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=$mainID ORDER BY name";
   $mainID = '';
-  $mainCats = $connection->query($sql);
+  
   //http://php.net/manual/en/pdo.query.php
   foreach ($connection->query($main_cat_sql) as $row){
     print $row['main_category_id'] . " ";
     print $row['name'] . "<br>";
     $mainID = $row['main_category_id'];
-    
-  foreach ($connection->query($sub_cat_sql) as $row){
+    $subArray = $connection->prepare($sub_cat_sql);
+    $subCats = $subArray->fetchAll();
+  foreach ($subCats as $row){
       print $row['sub_category_id'] . " ";
       print $row['name'] . "<br>";
     }
