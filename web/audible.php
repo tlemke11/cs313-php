fe2e705945e07c7c681f1366339922cf0553925b
tslemke11 <tyleris@gmail.com>
2016-10-22 22:09:21 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -14,37 +14,42 @@ $mainID = '';
     <link rel="stylesheet" type="css/txt" href="stylesheet.css">
   </head>
   <body>
-    <form action="signup.php">
+    <div id="container">
+      <h1>Audible Daily Deal Selector</h1>
+      <form action="signup.php">
 
-      <?php //This isn't super-clean - hopefully I can make it cleaner later
-      //http://php.net/manual/en/pdo.query.php
-      foreach ($connection->query($main_cat_sql) as $row){
-        $mainID = $row['main_category_id'];
-        $mainName = $row['name'];
-   
-        print "<input type='checkbox' name='mainCats' value='$mainID-$mainName'><strong>$mainName</strong><br>";
-        
-        //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
-        //See your common sense's answer
-        $subArray = $connection->prepare($sub_cat_sql);
-        $subArray->bindParam(':mainID', $mainID, PDO::PARAM_INT);
-        $subArray->execute();
-        $subCats = $subArray->fetchAll();
+        <?php //This isn't super-clean - hopefully I can make it cleaner later
+        //http://php.net/manual/en/pdo.query.php
+        foreach ($connection->query($main_cat_sql) as $row){
+          $mainID = $row['main_category_id'];
+          $mainName = $row['name'];
+     
+          print "<input type='checkbox' name='mainCats' value='$mainID-$mainName'><strong>$mainName</strong><br>";
           
-        foreach ($subCats as $rows){
-          $subID = $rows['sub_category_id'];
-          $subName = $rows['name'];
-          print "<input type='checkbox' name='subCats' value='$subID-$subName'><strong>$subName</strong><br>";
+          //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
+          //See your common sense's answer
+          $subArray = $connection->prepare($sub_cat_sql);
+          $subArray->bindParam(':mainID', $mainID, PDO::PARAM_INT);
+          $subArray->execute();
+          $subCats = $subArray->fetchAll();
+            
+          foreach ($subCats as $rows){
+            $subID = $rows['sub_category_id'];
+            $subName = $rows['name'];
+            print "<input type='checkbox' name='subCats' value='$subID-$subName'><strong>$subName</strong><br>";
+          }
+          print "<br>";
         }
-        print "<br>";
-      }
-      ?>
-      <br>
-      Email:<input type="email" name="email">
-      <br>
-      <input type="submit" value="Submit">
-    </form>
+        ?>
+        <br>
+        Email:<input type="email" name="email">
+        <br>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
 
-
+  <div id="footer">
+    Copyright Tyler Lemke 2016
+  <div>
   </body>
 </html>
