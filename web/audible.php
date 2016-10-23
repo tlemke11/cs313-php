bba89ab6573b8279e0bf120b9669074bcd6972e1
tslemke11 <tyleris@gmail.com>
2016-10-22 21:40:51 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -1,31 +1,43 @@
 <?php
 require_once('dbconnection.php'); //include the db
 
+//quieries
+$main_cat_sql = 'SELECT main_category_id, name FROM main_categories ORDER BY name';
+$sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=:mainID ORDER BY name";
+$mainID = '';
 
-//loop for querying and then displaying all of the subjects housed in the database.
-
-//this is gonna be a trip
-
-  $main_cat_sql = 'SELECT main_category_id, name FROM main_categories ORDER BY name';
-  $sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=:mainID ORDER BY name";
-  $mainID = '';
- 
-  //http://php.net/manual/en/pdo.query.php
-  foreach ($connection->query($main_cat_sql) as $row){
-    print $row['main_category_id'] . " ";
-    print $row['name'] . "<br>";
-    $mainID = $row['main_category_id'];
-    
-    //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
-    //See your common sense's answer
-    $subArray = $connection->prepare($sub_cat_sql);
-    $subArray->bindParam(':mainID', $mainID, PDO::PARAM_INT);
-    $subArray->execute();
-    $subCats = $subArray->fetchAll();
-    
-    foreach ($subCats as $rows){
-        print $rows['sub_category_id'] . " ";
-        print $rows['name'] . "<br>";
-      } 
-  }
 ?>
+
+<html>
+  <body>
+    <form action="signup.php">
+  
+
+      
+      <?php //This isn't super-clean - hopefully I can make it cleaner later
+      //http://php.net/manual/en/pdo.query.php
+      foreach ($connection->query($main_cat_sql) as $row){
+        print "<input type=\"checkbox\" name=\"mainCats\" value=\"" . $row['main_category_id'] . "-" $row['name'] . "\">$row['name']<br>";
+        $mainID = $row['main_category_id'];
+          
+        //http://stackoverflow.com/questions/15385965/php-pdo-with-foreach-and-fetch
+        //See your common sense's answer
+        $subArray = $connection->prepare($sub_cat_sql);
+        $subArray->bindParam(':mainID', $mainID, PDO::PARAM_INT);
+        $subArray->execute();
+        $subCats = $subArray->fetchAll();
+          
+        foreach ($subCats as $rows){
+          print $rows['sub_category_id'] . " ";
+          print $rows['name'] . "<br>";
+        }
+      }
+      ?>
+    
+    
+      <input type="submit value="Submit">
+    </form>
+
+
+  </body>
+</html>
