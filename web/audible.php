8997ae57c15c40454979138b6283899afc952004
tslemke11 <tyleris@gmail.com>
2016-10-22 21:10:51 -0500

    try try again

--- web/audible.php
+++ web/audible.php
@@ -10,13 +10,16 @@ require_once('dbconnection.php'); //include the db
   $sub_cat_sql = "SELECT sub_category_id,name,main_category_id FROM sub_categories WHERE main_category_id=117 ORDER BY name";
   $mainID = '';
   
+      $subArray = $connection->prepare($sub_cat_sql);
+    $subCats = $subArray->fetchAll();
+  
   //http://php.net/manual/en/pdo.query.php
   foreach ($connection->query($main_cat_sql) as $row){
     print $row['main_category_id'] . " ";
     print $row['name'] . "<br>";
     $mainID = $row['main_category_id'];
-    $subArray = $connection->prepare($sub_cat_sql);
-    $subCats = $subArray->fetchAll();
+    //$subArray = $connection->prepare($sub_cat_sql);
+   // $subCats = $subArray->fetchAll();
   foreach ($subCats as $rows){
       print $rows['sub_category_id'] . " ";
       print $rows['name'] . "<br>";
